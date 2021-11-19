936dbdf59179c9595d1676d454a70b3903cbb9e0
huafeng.huang <huafeng.huang@semidrive.com>
2021-11-19 10:58:25 +0800

    support record the cpu utilization for per cpu
    Signed-off-by: huafeng.huang <huafeng.huang@semidrive.com>

--- cpu_monitor.c
+++ cpu_monitor.c
@@ -112,6 +112,28 @@ int process_one_line(char *path, void (*cb)(char *line, void *data), void *data)
 	return ret;
 }
 
+static char *fmt_100percent_8(char pbuf[8], unsigned value, unsigned total)
+{
+	unsigned t;
+	if (value >= total) { /* 100% ? */
+		strcpy(pbuf, "  100% ");
+		return pbuf;
+	}
+	/* else generate " [N/space]N.N% " string */
+	value = 1000 * value / total;
+	t = value / 100;
+	value = value % 100;
+	pbuf[0] = ' ';
+	pbuf[1] = t ? t + '0' : ' ';
+	pbuf[2] = '0' + (value / 10);
+	pbuf[3] = '.';
+	pbuf[4] = '0' + (value % 10);
+	pbuf[5] = '%';
+	pbuf[6] = ' ';
+	pbuf[7] = '\0';
+	return pbuf;
+}
+
 static void parse_online_cpufreq_info(char *path)
 {
 	int offline_status = 0;
@@ -145,6 +167,115 @@ static void parse_online_cpufreq_info(char *path)
 #endif
 }
 
+static int read_cpu_jiffy(char *line, Jiffy_count_t *p_jif)
+{
+	static const char fmt[] = "cp%*s %llu %llu %llu %llu %llu %llu %llu %llu";
+	int ret;
+
+	ret = sscanf(line, fmt,
+		&p_jif->usr, &p_jif->nic, &p_jif->sys, &p_jif->idle,
+		&p_jif->iowait, &p_jif->irq, &p_jif->softirq,
+		&p_jif->steal);
+#ifdef DEBUG
+	printf("usr:%llu nic:%llu sys:%llu idle:%llu iowait:%llu irq:%llu \
+		sortirq:%llu steal:%llu\n", p_jif->usr, p_jif->nic, p_jif->sys,
+		 p_jif->idle, p_jif->iowait, p_jif->irq, p_jif->softirq,
+		 p_jif->steal);
+#endif
+	if (ret >= 4) {
+		p_jif->total = p_jif->usr + p_jif->nic + p_jif->sys + p_jif->idle
+			+ p_jif->iowait + p_jif->irq + p_jif->softirq + p_jif->steal;
+		/* Does not count iowait as busy time */
+		p_jif->busy = p_jif->total - p_jif->idle - p_jif->iowait;
+	}
+	return ret;
+}
+
+static int do_stat()
+{
+	FILE *file;
+	char *line = NULL;
+	ssize_t nread, len;
+	int ret = 0;
+	unsigned int cpu_id = 0;
+	Jiffy_count_t *p_cur_jiffy = systeminfo.cur_jiffy;
+	Jiffy_count_t *p_prev_jiffy = systeminfo.prev_jiffy;
+	char **p_rate = systeminfo.cpu_rate;
+
+
+	if (!p_cur_jiffy || !p_prev_jiffy || !p_rate) {
+		printf("The point of systeminfo.cur_jiffy is error\n");
+		return -ENOMEM;
+	}
+
+	memcpy((void *)p_prev_jiffy, (void *)p_cur_jiffy,
+			(systeminfo.nr_cpus + 1) * sizeof(Jiffy_count_t));
+
+	/* clear cur_jiffy buf */
+	memset(p_cur_jiffy, 0, (systeminfo.nr_cpus + 1) * sizeof(Jiffy_count_t));
+
+	file = fopen(STAT_PATH, "r");
+	if (!file) {
+		printf("Need to support /proc/stat\n");
+		return -EINVAL;
+	}
+
+	/* Only need CPU info */
+	while ((nread = getline(&line, &len, file)) != -1 && line[0] == 'c') {
+		char *p_buf;
+
+		if (!strncmp(line, "cpu ", strlen("cpu "))) {
+			/* First line */
+			ret = read_cpu_jiffy(line, &p_cur_jiffy[0]);
+			if (ret < 0) {
+				printf("read /proc/stat failed\n");
+				return -EINVAL;
+			}
+			continue;
+		}
+
+		p_buf = &line[3];
+		ret = sscanf(p_buf, "%u", &cpu_id);
+		if (ret < 0) {
+			printf("get cpu id form /proc/stat failed\n");
+			return -EINVAL;
+		}
+#ifdef DEBUG
+		printf("cpu_id:%u\n", cpu_id);
+#endif
+		/*Get all online cpu jify */
+		ret = read_cpu_jiffy(line, &p_cur_jiffy[cpu_id + 1]);
+		if (ret < 0) {
+			printf("read /proc/stat failed\n");
+			return -EINVAL;
+		}
+
+		/* compare p_cur_jiffy[cpu_id+1].idle with
+		 * p_prev_jiffy[cpu_id+1].idle for hotplug
+		 */
+		if (p_cur_jiffy[cpu_id + 1].idle < p_prev_jiffy[cpu_id + 1].idle) {
+			/* cpu hotplug happen when sample
+			 * assume the cpu in dile, so the cpu utilization is 0
+			 */
+			fmt_100percent_8(p_rate[cpu_id], 0, 1);
+		} else {
+			/*
+			 * cpu% = (cur_jif.busy - prev_jif.busy) / (cur_jif.total - prev_jif.total) * 100%
+			*/
+			unsigned total_diff, busy_diff;
+			total_diff = (unsigned)(p_cur_jiffy[cpu_id + 1].total - p_prev_jiffy[cpu_id + 1].total);
+			busy_diff = (unsigned)(p_cur_jiffy[cpu_id + 1].busy - p_prev_jiffy[cpu_id + 1].busy);
+			if (total_diff == 0)
+				total_diff = 1;
+			fmt_100percent_8(p_rate[cpu_id], busy_diff, total_diff);
+		}
+	}
+
+	free(line);
+	fclose(file);
+	return 0;
+}
+
 static int parse_cpu_info(void)
 {
 	char new_path[PATH_MAX];
@@ -161,6 +292,18 @@ static int parse_cpu_info(void)
 #endif
 		parse_online_cpufreq_info(new_path);
 	}
+
+	/* calc the cpu utilization for per cpu */
+	if (systeminfo.first_run_flag) {
+		struct timespec tv;
+		systeminfo.first_run_flag = 0;
+		tv.tv_sec = 0;
+		tv.tv_nsec = 500000000;	//500ms
+		do_stat();
+		nanosleep(&tv, NULL);
+	}
+	do_stat();
+
 	return 0;
 }
 
@@ -178,7 +321,7 @@ static int parse_master_tempinfo(char *path)
 	file = fopen(new_path, "r");
 	if (!file) {
 		printf("No such file:%s", new_path);
-		return ret;
+		return -EINVAL;
 	}
 
 	snprintf(new_path, ADJ_SIZE(PATH_MAX, path, "/temp"), "%s/temp", path);
@@ -237,12 +380,15 @@ static int parse_system_master_temp_info()
 				parse_master_tempinfo(new_path);
 			}
 		} while (entry);
+
 	closedir(dir);
+
 	return 0;
 }
 
 static int init_systeminfo_struct(struct systeminfo *systeminfo)
 {
+	int i = 0;
 
 	/* Get total cpu nums */
 	systeminfo->nr_cpus = sysconf(_SC_NPROCESSORS_CONF);
@@ -276,31 +422,77 @@ static int init_systeminfo_struct(struct systeminfo *systeminfo)
 		}
 	}
 
+	systeminfo->first_run_flag = 1;
+	/*
+	 * porc/stat format:
+	 *      usr nic sys idle iowait irq softirq steal guest guest_nice
+	 * cpu  1338573 0  1382987 28101161 15 324191 200193 0 0 0
+	 * cpu0 330964 0 586465 7592474 11 202696 77311 0 0 0
+	 * cpun 4983 0 111429 10184037 1 44124 44965 0 0 0
+	 */
+	systeminfo->cur_jiffy = (Jiffy_count_t *)malloc((systeminfo->nr_cpus + 1) * sizeof(Jiffy_count_t));
+	systeminfo->prev_jiffy = (Jiffy_count_t *)malloc((systeminfo->nr_cpus + 1) * sizeof(Jiffy_count_t));
 	systeminfo->cpufreq = (unsigned int *)malloc(systeminfo->nr_cpus * sizeof(unsigned int));
-	if (!systeminfo->cpufreq) {
-		printf("alloc mem for systeminfo cpufreq failed\n");
+	systeminfo->cpu_rate = (char **)malloc(systeminfo->nr_cpus * sizeof(char *));
+
+	if (!systeminfo->cpufreq || !systeminfo->cur_jiffy
+		|| !systeminfo->prev_jiffy || !systeminfo->cpu_rate) {
+		printf("alloc mem for systeminfo failed\n");
 		return -ENOMEM;
 	}
 
+	for(i = 0; i < systeminfo->nr_cpus; i++) {
+		systeminfo->cpu_rate[i] = (char *)malloc(8 * sizeof(char));
+		if (!systeminfo->cpu_rate[i]) {
+			printf("alloc mem for cpu rate failed\n");
+			return -ENOMEM;
+		}
+	}
+
+	memset((void *)systeminfo->cur_jiffy, 0, (systeminfo->nr_cpus + 1) * sizeof(Jiffy_count_t));
+	memset((void *)systeminfo->prev_jiffy, 0, (systeminfo->nr_cpus + 1) * sizeof(Jiffy_count_t));
+	memset((void *)systeminfo->cpufreq, 0, systeminfo->nr_cpus * sizeof(unsigned int));
+	for(i = 0; i < systeminfo->nr_cpus; i++)
+		memset((void *)systeminfo->cpu_rate[i], 0, 8 * sizeof(char));
+
 	return 0;
+}
+
+static void destroy_systeminfo_struct()
+{
+	int i;
+
+	if(systeminfo.cur_jiffy)
+		free(systeminfo.cur_jiffy);
+	if (systeminfo.prev_jiffy)
+		free(systeminfo.prev_jiffy);
+	if (systeminfo.cpufreq)
+		free(systeminfo.cpufreq);
+
+	for(i = 0; i < systeminfo.nr_cpus; i++)
+		if (systeminfo.cpu_rate[i])
+			free(systeminfo.cpu_rate[i]);
+
+	if (systeminfo.cpu_rate)
+		free(systeminfo.cpu_rate);
 }
 
 static void display_header(void)
 {
 	printf("System info:\n");
-	printf("\t\tCPU%%\t\tcpufreq(kHz)\t\ttemp\t\tgpufreq(kHz)\t\tgputemp\n");
+	printf("\tCPU%%\t\tcpufreq(kHz)\t\ttemp\t\tgpufreq(kHz)\t\tgputemp\n");
 }
 
 static void display_system_info(void)
 {
-	static const char fmt[] = "cpu%d\t\t%4u\t\t%12u\t\t%4u\t\t%12u\t\t%4u\n";
+	static const char fmt[] = "cpu%d\t%s\t\t%12u\t\t%4u\t\t%12u\t\t%4u\n";
 	char line_buf[LINE_BUF_SIZE];
 	int ret;
 	unsigned int i;
 
 	for(i = 0; i < systeminfo.nr_cpus; i++) {
 		for_each_online_cpu(i) {
-			ret = sprintf(line_buf, fmt, i, 0, systeminfo.cpufreq[i],
+			ret = sprintf(line_buf, fmt, i, systeminfo.cpu_rate[i], systeminfo.cpufreq[i],
 					systeminfo.cpu_temp, 0, systeminfo.gpu_temp);
 			fputs(line_buf, stdout);
 			fflush(NULL);
@@ -340,5 +532,6 @@ int main(int argc, char *argv[])
 		}
 		nanosleep(&tv, NULL);
 	}
+	destroy_systeminfo_struct();
 	return 0;
 }
--- cpu_monitor.h
+++ cpu_monitor.h
@@ -4,14 +4,25 @@
 #define PATH_MAX	4096	/* # chars in a path name including nul */
 #define CPU_PATH	"/sys/devices/system/cpu"
 #define THERMAL_PATH	"/sys/devices/virtual/thermal"
+#define STAT_PATH	"/proc/stat"
 #define ADJ_SIZE(l,r,s) (l-strlen(r)-strlen(#s))
 #define LINE_BUF_SIZE	1024
 
+typedef struct jiffy_counts_t {
+	unsigned long long usr, nic, sys, idle;
+	unsigned long long iowait, irq, softirq, steal;
+	unsigned long long total;
+	unsigned long long busy;
+}Jiffy_count_t;
+
 typedef struct systeminfo {
+	int		first_run_flag;
 	unsigned int	*cpufreq;		//cpu current freq info
 	unsigned int	nr_cpus;		//total cpus num
 	unsigned int	cpu_temp;
 	unsigned int	gpu_temp;
+	Jiffy_count_t *cur_jiffy, *prev_jiffy;
+	char	**cpu_rate;
 }Systeminfo_t;
 
 #endif
